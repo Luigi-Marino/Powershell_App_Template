17bc5b5661480f04913b659494216616d85d0789
Luigi Marino <emails@luigi-marino.com>
2026-05-05 10:42:22 +0100

    Basic initial build of template

--- powershell_app_template.ps1
+++ powershell_app_template.ps1
@@ -1,1 +1,34 @@
+# Minimal GitHub App Template
 
+# Configuration
+$Repo = ""
+$RepoBase = "https://raw.githubusercontent.com/Luigi-Marino/$Repo/main"
+$ModuleNames = @(
+    "template_module.psm1",
+    "ModuleB.psm1"
+)
+
+# Module Loader
+function Load-RemoteModules {
+    param($BaseURL, $Modules)
+
+    foreach ($m in $Modules) {
+        $url = ""$BaseURL/modules/$m"
+        $code = Invoke-RestMethod $url
+
+        $mod = New-Module -ScriptBlock ([ScriptBlock]::Create($code)) -Name $m
+        Import-Module $mod -Force
+    }
+}
+
+# Detect Execution Method
+if ([string]::IsNullOrWhiteSpace($PSScriptRoot)) {
+    Load-RemoteModules -BaseUrl $RepoBase -Modules $ModuleNames
+}
+else {
+    Get-ChildItem "$PSScriptRoot/modules" -Filter *.psm1 |
+        ForEach-Object { Import-Module $_.FullName -Force }
+}
+
+# Entry Point
+Main
